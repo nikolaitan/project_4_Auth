66bcc88dfeb0b434ed3babec900b2623cc233519
nikolaitan <573901070@qq.com>
2025-11-27 18:56:14 +0800

    Initial commit

---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    login_history = db.relationship('LoginHistory', backref='user', lazy=True)
+
+class LoginHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    login_time = db.Column(db.DateTime, default=datetime.utcnow)
+    user_agent = db.Column(db.String(200))
+    ip = db.Column(db.String(45))
